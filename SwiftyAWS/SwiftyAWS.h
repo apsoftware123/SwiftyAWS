56ab1057316874d06804fe0a5908cfe00787a5de
Andres Peguero <apeguero24@gmail.com>
2017-10-28 09:07:39 -0400

    Initial Commit

---
+++ SwiftyAWS/SwiftyAWS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyAWS.h
+//  SwiftyAWS
+//
+//  Created by Andres Peguero on 10/28/17.
+//  Copyright © 2017 Andres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyAWS.
+FOUNDATION_EXPORT double SwiftyAWSVersionNumber;
+
+//! Project version string for SwiftyAWS.
+FOUNDATION_EXPORT const unsigned char SwiftyAWSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyAWS/PublicHeader.h>
+
+
